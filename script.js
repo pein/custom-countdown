bb7ccf6ce35957c20d8e6b8aba4f5f012dd750fc
Ilyes Houha <mi.houha@gmail.com>
2020-12-15 01:47:45 +0100

    Getting date & title

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const inputContiner = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+let countdownTitle = "";
+let coundownDate = "";
 //  Set Date Input Minimum
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
+
+// Take values from form
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  console.log(countdownDate);
+}
+
+// Event Listners
+countdownForm.addEventListener("submit", updateCountdown);
